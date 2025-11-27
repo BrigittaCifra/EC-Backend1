d2aa3a0299855702be88ecc48c3826386f2e7a3a
Brigitta Cifra <brigittacifra@hotmail.com>
2025-11-27 15:26:59 +0100

    gjorde en route för att söka på namnet på kategorierna

--- src/repositories/categoriesRepository.js
+++ src/repositories/categoriesRepository.js
@@ -29,3 +29,25 @@ export async function getAllCategories(limit, offset) {
     //result.rows kommer att returnera själva datan/innehållet
     return [totalAmount, result.rows];
 }
+
+export async function getCategoryByName(name, limit, offset) {
+
+    //LOWER(name) konverterar kategori namnet i databasen
+    //wildcard karaktärerna % matchar alla tecken före och efter söksträngen
+    //name.toLowerCase() konverterar söksträngen så att sökningen inte blir case sensitive
+    const result = await pool.query(`SELECT categories.*,
+        count(*) OVER() AS full_count  
+        FROM categories 
+        WHERE LOWER(categories.name) LIKE $1
+        LIMIT $2 OFFSET $3`,
+        ['%' + name.toLowerCase() + '%', limit, offset]
+    );
+
+    const totalAmount = parseInt(result.rows[0].full_count);
+
+    for (let rows of result.rows) {
+        delete rows.full_count
+    }
+
+    return [totalAmount, result.rows];
+}
--- src/routes/categories.js
+++ src/routes/categories.js
@@ -4,7 +4,7 @@ const router = express.Router();
 import { numValidator, stringValidator, emailValidator, phonenumberValidator } from '../middlewares/validators.js'
 import { AppError, ValidationError, NotFoundError } from '../middlewares/errorClasses.js'
 import { asyncWrapper } from '../utils/tryCatch.js';
-import { getAllCategories } from '../repositories/categoriesRepository.js';
+import { getAllCategories, getCategoryByName } from '../repositories/categoriesRepository.js';
 
 //Hämtar alla kategorier
 router.get('/', asyncWrapper(async (req, res) => {
@@ -32,6 +32,30 @@ router.get('/', asyncWrapper(async (req, res) => {
         Total_antal_produkter: totalAmount,
         //Här används result.length eftersom det kan hända att det finns färre resultat än
         //vad limit vill begränsa det till. Pga detta behöver denna resultat visas dynamiskts
+        Antal_kategorier_som_visas: result.length,
+        Sida: page,
+        Kategorier: result
+    });
+
+}));
+
+//Hämtar kategorier baserat på namn
+router.get('/search', asyncWrapper(async (req, res) => {
+    const nameSearch = stringValidator(req.query.name, 'name');
+
+    const page = numValidator(req.query.page || 1);
+    const limit = numValidator(req.query.limit || 3);
+
+    const offset = (page - 1) * limit;
+
+    const [totalAmount, result] = await getCategoryByName(nameSearch, limit, offset);
+
+    if (result.length === 0) {
+        throw new NotFoundError('Inga kategorier hittades');
+    }
+
+    res.json({
+        Hittade_kategorier: totalAmount,
         Antal_kategorier_som_visas: result.length,
         Sida: page,
         Kategorier: result
